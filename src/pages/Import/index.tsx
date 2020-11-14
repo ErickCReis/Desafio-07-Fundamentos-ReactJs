4b76aaba79df1a8eed0ddd473a9abed93f755762
ErickCReis <erickcorreareis@gmail.com>
2020-11-13 22:48:31 -0300

    Adiciona resolução do desafio

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    if (!uploadedFiles.length) return;
+
+    uploadedFiles.forEach(file => {
+      data.append('file', file.file, file.name);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.goBack();
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const recievedFiles: FileProps[] = files.map(file => {
+      return { file, name: file.name, readableSize: filesize(file.size) };
+    });
+
+    setUploadedFiles([...uploadedFiles, ...recievedFiles]);
   }
 
   return (
